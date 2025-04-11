560fd95dc2e33644d469e20530e930f674e92c2e
Bernardo Olmedo <bolmedo@uade.edu.ar>
2025-04-10 21:49:19 -0300

    avances en la logica

--- wordle.py
+++ wordle.py
@@ -1,39 +1,44 @@
 import random
 
-def comparar_palabras(lista1,lista2):
-        for i in range(len(lista1)):
-                if lista1[i]==lista2[i]:
-                        print(lista1[i], "es correcto")
-                elif lista1[i] in lista2:
-                        print(lista1[i], "está en la palabra pero en la posición incorrecta")
-                else:
-                        print(lista1[i], "no está en la palabra")
-                
+palabra=[]
+intento=[]
 
-
-
-def logica():
+def logica(palabra, intento):
         palabras=["dedos","manos","torso","silla", "llave", "mundo", "canto", "nieve", "lento", "campo", "plomo", "tigre", "brisa"]
-        palabra=[]
-        palabra.extend(palabras[random.randint(0,len(palabras)-1)])
-        intento=[]
-        intentos=str(input("Ingresa tu intento de 5 letras: "))
-        while len(intentos)!=5:
-                print("El intento debe contener exactamente 5 letras.")
-                intentos=str(input("Ingresa tu intento de 5 letras: "))
+        palabraSecreta=palabras[random.randint(0,len(palabras)-1)]
+        palabra.extend(palabraSecreta)
         
-        intento.extend(intentos)
+
+        bandera=True        
+        while bandera: 
+                intento.clear()
+                intentos=str(input("Ingresa tu intento de 5 letras: "))
                 
-        while intento!=palabra: 
-                print("Palabra incorrecta.")
-                intento=[]
-                intentos=str(input("Ingresa tu intento de 5 letras: "))
-                intento.extend(intentos)
+                if len(intentos)!=5:
+                        print("El intento debe contener exactamente 5 letras.")
+                else:
+                        intento.clear()
+                        intento.extend(intentos) 
+                
+                        if intento==palabra:
+                                print("Felicidades, has adivinado la palabra!")
+                                bandera=False
+                        else:
+                                for i in range(5):
+                                        if intento[i] == palabra[i]:
+                                                print(intento[i], "está en la palabra y en la posición correcta")
+                                        elif intento[i] in palabra:
+                                                print(intento[i], "está en la palabra pero en la posición incorrecta")
+                                        else:
+                                                print(intento[i], "no está en la palabra")
+
+        return palabra, intento
+                
+
+
+palabra, intento=logica(palabra, intento)
         
-        print("Felicitaciones, adivinaste la palabra")
-      
-logica()
         
         
-        
+
         
