ea2cec25d6a4470f6ff47ab6c10bde66195302c6
AlejoFC08 <163475473+AlejoFC08@users.noreply.github.com>
2025-04-14 13:45:44 -0300

    Funcion logica y cont de intentos
    Se agrego la funcion "Jugar" dentro de la funcion "Logica"
    Se agrgo la funcion seguir_jugando para controlar si quiere seguir jugando 
    Se agrego el contantador y  controlador de intentos (6)

--- wordle.py
+++ wordle.py
@@ -1,44 +1,61 @@
 import random
 
-palabra=[]
-intento=[]
+palabra = []
+intento = []
 
 def logica(palabra, intento):
-        palabras=["dedos","manos","torso","silla", "llave", "mundo", "canto", "nieve", "lento", "campo", "plomo", "tigre", "brisa"]
-        palabraSecreta=palabras[random.randint(0,len(palabras)-1)]
+    palabras = ["dedos", "manos", "torso", "silla", "llave", "mundo", "canto", "nieve", "lento", "campo", "plomo", "tigre", "brisa"]
+    seguir_jugando = True
+
+    while seguir_jugando:
+        palabra.clear()
+        intento.clear()
+        palabraSecreta = palabras[random.randint(0, len(palabras) - 1)]
         palabra.extend(palabraSecreta)
-        
 
-        bandera=True        
-        while bandera: 
-                intento.clear()
-                intentos=str(input("Ingresa tu intento de 5 letras: "))
-                
-                if len(intentos)!=5:
-                        print("El intento debe contener exactamente 5 letras.")
+        print("Si no encuentra la palabra, escriba la palabra VENCIDO")
+        print("Tenés hasta 6 intentos para adivinar la palabra.")
+
+        bandera = True        
+        cantidad_intentos = 0
+
+        while bandera and cantidad_intentos < 6: 
+            intento.clear()
+            numero_intento = cantidad_intentos + 1
+            mensaje = f"Intento {numero_intento}/6 - Ingresa tu intento de 5 letras: "
+            intentos = input(mensaje)
+
+            if (intentos == "VENCIDO") or (intentos == "Vencido") or (intentos == "vencido"):
+                        print("Te rendiste. La palabra era:", "".join(palabra))
+                        break
+
+            if len(intentos) != 5:
+                print("El intento debe contener exactamente 5 letras.")
+            else:
+                intento.extend(intentos)
+                cantidad_intentos += 1
+
+                if intento == palabra:
+                    print("Felicidades, has adivinado la palabra!")
+                    bandera = False
                 else:
-                        intento.clear()
-                        intento.extend(intentos) 
-                
-                        if intento==palabra:
-                                print("Felicidades, has adivinado la palabra!")
-                                bandera=False
+                    for i in range(5):
+                        if intento[i] == palabra[i]:
+                            print(intento[i], "está en la palabra y en la posición correcta")
+                        elif intento[i] in palabra:
+                            print(intento[i], "está en la palabra pero en la posición incorrecta")
                         else:
-                                for i in range(5):
-                                        if intento[i] == palabra[i]:
-                                                print(intento[i], "está en la palabra y en la posición correcta")
-                                        elif intento[i] in palabra:
-                                                print(intento[i], "está en la palabra pero en la posición incorrecta")
-                                        else:
-                                                print(intento[i], "no está en la palabra")
+                            print(intento[i], "no está en la palabra")
 
-        return palabra, intento
-                
+        if cantidad_intentos == 6 and intento != palabra:
+            print("Se acabaron los intentos. La palabra era:", "".join(palabra))
 
+        respuesta = input("Querés jugar de nuevo? SI/NO: ")
+        if respuesta not in ("SI", "Si", "si"):
+            seguir_jugando = False
+            print("Gracias por jugar!")
+        else:
+            print("Comencemos de nuevo!")
 
-palabra, intento=logica(palabra, intento)
-        
-        
-        
-
-        
+# Ejecutar el juego
+logica(palabra, intento)
