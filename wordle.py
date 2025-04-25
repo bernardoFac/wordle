9bffced8b39bbcc1067645e86283bf3785b5624e
Bernardo Olmedo <bolmedo@uade.edu.ar>
2025-04-25 09:47:51 -0300

    Update wordle.py
    agregacion de la seleccion de categorias incluyendo el uso de matrices

--- wordle.py
+++ wordle.py
@@ -2,50 +2,64 @@ import random
 
 palabra = []
 intento = []
+clubes=["Milan", "River", "Inter", "Colon", "Union", "Vélez", "Betis","Porto","Genoa","Celta"]
+colores=[ "verde", "negro","beige","grana"]
+paises=["Chile", "Japon", "Qatar", "Nepal", "India", "Siria"]	
+palabras = [clubes, colores, paises]
 
-def logica(palabra, intento):
-    palabras = ["dedos", "manos", "torso", "silla", "llave", "mundo", "canto", "nieve", "lento", "campo", "plomo", "tigre", "brisa"]
-    palabraSecreta = palabras[random.randint(0, len(palabras) - 1)]
+def seleccionar_categoria(palabras):
+    for i in range(len(palabras)):
+        nombre_categoria = ["Clubes", "Colores", "Países"]
+        print(f"{i + 1}. {nombre_categoria[i]}")    
+    seleccion = int(input("Ingresa el número de la categoría que deseas jugar: ")) - 1
+    if seleccion < 0 or seleccion >2:
+        print("Selección inválida. Se elegirá una categoría aleatoria.")
+        seleccion = random.randint(0, len(palabras) - 1)
+    else:
+        print(f"Has seleccionado la categoría: {palabras[seleccion]}")
+        
+    return palabras[seleccion]
+
+def logica(palabras, intento):
+    palabraSecreta = palabras[random.randint(0, len(palabras) - 1)].lower()
     palabra.clear()
     palabra.extend(palabraSecreta)
-
-    print("Si no encuentra la palabra, escriba la palabra VENCIDO")
-
-    bandera = True        
+    bandera=True        
     while bandera: 
-        intento.clear()
-        intentos = str(input("Ingresa tu intento de 5 letras: "))
+                intento.clear()
+                intentos=str(input("Ingresa tu intento de 5 letras o vencido para dejar de jugar: ")).lower()
+                
+                if intentos == "vencido":
+                        print("La palabra secreta era: ", palabraSecreta)
+                        bandera=False
+                if len(intentos)!=5:
+                        print("El intento debe contener exactamente 5 letras.")
+                else:
+                        intento.clear()
+                        intento.extend(intentos) 
+                
+                        if intento==palabra:
+                                print("Felicidades, has adivinado la palabra!")
+                                bandera=False
+                        else:
+                                for i in range(5):
+                                        if intento[i] == palabra[i]:
+                                                print(intento[i], "está en la palabra y en la posición correcta")
+                                        elif intento[i] in palabra:
+                                                print(intento[i], "está en la palabra pero en la posición incorrecta")
+                                        else:
+                                                print(intento[i], "no está en la palabra")
+    return palabra, intento 
 
-        if intentos.lower() == "vencido":
-            print("Te rendiste. La palabra era:", palabraSecreta)
-            break
-
-        if len(intentos) != 5:
-            print("El intento debe contener exactamente 5 letras.")
-        else:
-            intento.clear()
-            intento.extend(intentos.lower())  
-
-            if intento == palabra:
-                print("Felicidades, has adivinado la palabra!")
-                bandera = False
-            else:
-                for i in range(5):
-                    if intento[i] == palabra[i]:
-                        print(intento[i], "está en la palabra y en la posición correcta")
-                    elif intento[i] in palabra:
-                        print(intento[i], "está en la palabra pero en la posición incorrecta")
-                    else:
-                        print(intento[i], "no está en la palabra")
-
-    return palabra, intento
 
 def jugar():
     while True:
-        logica(palabra, intento)
-        respuesta = input("Querés jugar de nuevo? SI/NO: ")
+        categoria = seleccionar_categoria(palabras)
+        if categoria:
+            logica(categoria, intento)
+        respuesta = input("¿Querés jugar de nuevo? SI/NO: ")
         if respuesta.lower() != "si":
-            print("Gracias por jugar!")
+            print("¡Gracias por jugar!")
             break
 
 jugar()
