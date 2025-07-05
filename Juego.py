94efa79e338ef0c96e3412528e7066d59b443357
MatiasBento20 <matiasbento20@gmail.com>
2025-07-05 18:09:31 -0300

    Se creó y se implementó la función crearRanking

--- Juego.py
+++ Juego.py
@@ -4,7 +4,7 @@ import os
 
 palabra = []
 # Distintas listas que contienen a las palabras que conforman la categoria
-clubes = ("Milan", "River", "Inter", "Colon", "Union", "Velez", "Betis", "Roma", "Porto", "Genoa", "Celta", "Chelsea")  
+clubes = ("Milan", "River", "Inter", "Colon", "Union", "Velez", "Betis", "Roma", "Porto", "Genoa", "Celta", "Chelsea", "Boca")  
 colores = ("verde", "negro", "beige", "grana", "rojo", "azul", "gris", "cyan", "amarillo") 
 paises = ("Chile", "Japon", "Qatar", "Nepal", "India", "Siria", "Peru", "Brasil", "España")  
 
@@ -42,8 +42,8 @@ def seleccionar_categoria(palabras):
         seleccion = -1
 
     if seleccion < 0 or seleccion >= len(palabras):
-        print("Selección inválida. Se elegirá una categoría aleatoria.")
         seleccion = random.randint(0, len(palabras) - 1)
+        print(f"Selección inválida. Se elegirá una categoría aleatoria: {nombre_categoria[seleccion]}")
     else:
         print(f"Has seleccionado la categoría: {nombre_categoria[seleccion]}")
         
@@ -184,10 +184,46 @@ def actualizarHistorial(dni,nombre,aciertos):
             file.writelines(nuevas_lineas)
     except Exception as e:
         print(f"Ocurrio un error {e}")
- 
+
+# actualiza el ranking de los mejores 5 jugadores del juejo
+def crearRanking():
+    ruta_actual = os.path.dirname(__file__)
+    ruta_historial = os.path.join(ruta_actual, "historial.csv")
+    ruta_ranking = os.path.join(ruta_actual, "ranking.txt")
+
+    jugadores = []
+
+    try:
+        with open(ruta_historial, "r") as file:
+            for linea in file:
+                dni, nombre, aciertos = linea.strip().split(";")
+                jugadores.append({"dni": dni, "nombre": nombre, "aciertos": int(aciertos)})
+    except Exception as e:
+        print(f"No se pudo leer el historial: {e}")
+        return
+
+    jugadores.sort(key=lambda x: x["aciertos"], reverse=True)
+    top_5 = jugadores[:5]
+
+    print("🏆 TOP 5:")
+    i = 1
+    for jugador in top_5:
+        print(f"{i}. {jugador['nombre']} (DNI: {jugador['dni']}) - Aciertos: {jugador['aciertos']}")
+        i += 1
+
+    try:
+        with open(ruta_ranking, "w") as file:
+            i = 1
+            for jugador in top_5:
+                file.write(f"{i}. {jugador['nombre']} (DNI: {jugador['dni']}) - Aciertos: {jugador['aciertos']}\n")
+                i += 1
+    except Exception as e:
+        print(f"No se pudo escribir el ranking: {e}")
+
  
 # funcion principal que registra al usuario y va llamando a las otras funciones       
 def jugar():
+    triunfos = 0
     dificultad = pedir_dificultad()
 
     longitud = 4 if dificultad == "1" else 5 if dificultad == "2" else 7
@@ -199,7 +235,14 @@ def jugar():
     print("║         🎉  BIENVENIDO A...          ║")
     print("║             🌟 WORDLE 🌟             ║")
     print("╚══════════════════════════════════════╝\n")
-    nombre = input("Ingresa tu nombre: ")
+    
+    while True:
+        nombre = input("Ingresa tu nombre: ")
+        if nombre.isalpha():
+            break
+        else:
+            print("El nombre solo debe contener letras. Intentá de nuevo.")
+
     while True:
         try:
             dni = int(input("Ingresa tu dni: "))
@@ -212,6 +255,7 @@ def jugar():
     total_intentos_exitosos = 0
     victorias = 0
 
+
     while True:
         categoria = seleccionar_categoria(palabras_filtradas)
 
@@ -222,28 +266,134 @@ def jugar():
             if gano:
                 victorias += 1
                 total_intentos_exitosos += intentos_realizados
+                triunfos += 1
+                if triunfos == 2:
+                    print("🔥¡Dos triunfos seguidos!")
+                elif triunfos == 3:
+                    print("💥¡Tres triunfos consecutivos! No te para nadie!")
+                elif triunfos >= 5:
+                    print("🏆¡Crack total!")
+            else:
+                triunfos = 0
 
         respuesta = input("¿Querés jugar de nuevo? SI/NO: ")
         if respuesta.lower() != "si":
             actualizarHistorial(dni, nombre, aciertos)
             print("\n🎮 Gracias por jugar a Wordle. ¡Hasta la próxima! 🎮")
 
-            if partidas > 0:
-                porcentaje = (victorias / partidas) * 100
-                promedio = total_intentos_exitosos / victorias if victorias > 0 else 0
-                print("Estadísticas:")
-                print(f"Partidas jugadas: {partidas}")
-                print(f"Victorias: {victorias}")
-                print(f"Porcentaje de victorias: {porcentaje:.2f}%")
-                print(f"Promedio de intentos por victoria: {promedio:.2f}")
-            else:
-                print("No se jugaron partidas.")
+            intentos_estadisticas = 0
+            ver_estadisticas = ""
+
+            while intentos_estadisticas < 2:
+                ver_estadisticas = input("¿Querés ver tus estadísticas de juego? SI/NO: ").lower()
+                if ver_estadisticas in ("si", "no"):
+                    break
+                else:
+                    intentos_estadisticas += 1
+                    print("Respuesta inválida. Por favor escribí 'SI' o 'NO'.")
+
+            
+            if intentos_estadisticas == 2 and ver_estadisticas not in ("si", "no"):
+                print("Respuesta automática: NO")
+                ver_estadisticas = "no"
+
+            if ver_estadisticas == "si":
+                if partidas > 0:
+                    porcentaje = (victorias / partidas) * 100
+                    promedio = total_intentos_exitosos / victorias if victorias > 0 else 0
+                    print("Estadísticas:")
+                    print(f"Partidas jugadas: {partidas}")
+                    print(f"Victorias: {victorias}")
+                    print(f"Porcentaje de victorias: {porcentaje:.2f}%")
+                    print(f"Promedio de intentos por victoria: {promedio:.2f}")
+                else:
+                    print("No se jugaron partidas.")
 
             leerHistorial()
+            intentos_ranking = 0
+            ver_ranking = ""
+
+            while intentos_ranking < 2:
+                ver_ranking = input("¿Querés ver el ranking TOP 5? SI/NO: ").lower()
+                if ver_ranking in ("si", "no"):
+                    break
+                else:
+                    intentos_ranking += 1
+                    print("Respuesta inválida. Por favor escribí 'SI' o 'NO'.")
+
+            if intentos_ranking == 2 and ver_ranking not in ("si", "no"):
+                print("Respuesta automática: NO")
+                ver_ranking = "no"
+
+            if ver_ranking == "si":
+                crearRanking()
             break
+
+def jugar_duelo():
+    """Modo Duelo donde se pueden enfrentar 2 usuarios localmente y ver quien es el mejor"""
+    print("🔥Modo de Duelo🔥")
+    nombre1 = input("Jugador 1 - Ingresá tu nombre: ")
+    dni1 = input("Jugador 1 - Ingresá tu DNI: ")
+    nombre2 = input("Jugador 2 - Ingresá tu nombre: ")
+    dni2 = input("Jugador 2 - Ingresá tu DNI: ")
+
+    dificultad = pedir_dificultad()
+    longitud = 4 if dificultad == "1" else 5 if dificultad == "2" else 7
+
+    palabras_filtradas = []
+    for categoria in palabras_ordenadas:
+        palabras_filtradas.append(tuple(p for p in categoria if len(p) == longitud))
+
+    print(f"\n Turno de {nombre1}")
+    categoria1 = seleccionar_categoria(palabras_filtradas)
+    palabra_secreta1 = generar_palabra_secreta(categoria1)
+    intento1 = []
+    aciertos1 = 0
+    _, _, aciertos1, gano1, intentos1 = logica([palabra_secreta1], intento1, aciertos1)
+
+    print(f"\n Turno de {nombre2}")
+    categoria2 = seleccionar_categoria(palabras_filtradas)
+    palabra_secreta2 = generar_palabra_secreta(categoria2)
+    intento2 = []
+    aciertos2 = 0
+    _, _, aciertos2, gano2, intentos2 = logica([palabra_secreta2], intento2, aciertos2)
+
+    print("\n Resultado Del Duelo:")
+    if gano1 and not gano2:
+        print(f"🏆 ¡Gana {nombre1}!")
+    elif gano2 and not gano1:
+        print(f"🏆 ¡Gana {nombre2}!")
+    elif gano1 and gano2:
+        if intentos1 < intentos2:
+            print(f"🏆 Ambos adivinaron, pero {nombre1} lo hizo en menos intentos.")
+        elif intentos2 < intentos1:
+            print(f"🏆 Ambos adivinaron, pero {nombre2} lo hizo en menos intentos.")
+        else:
+            print("🤝 ¡Empate! Ambos adivinaron en la misma cantidad de intentos.")
+    else:
+        print("💀 Ninguno adivinó, son malisimos")
+
+    if gano1:
+        actualizarHistorial(dni1, nombre1, aciertos1)
+    if gano2:
+        actualizarHistorial(dni2, nombre2, aciertos2)
+        
+    print(f"\nPalabra de {nombre1} era: {palabra_secreta1}")
+    print(f"Palabra de {nombre2} era: {palabra_secreta2}")
+    print("📂 Resultados guardados")
+    print("\n🎮 Gracias por jugar a Wordle. ¡Hasta la próxima! 🎮")
     
 
 # main que ejecuta la funcion principal        
 if __name__ == "__main__":
-    jugar()
+    print("╔══════════════════════════════════════╗")
+    print("║         🎉  BIENVENIDO A...          ║")
+    print("║             🌟 WORDLE 🌟             ║")
+    print("╚══════════════════════════════════════╝\n")
+    modo = input("🎮 ¿Querés jugar solo o en duelo? (solo/duelo): ").lower()
+    if modo == "duelo":
+        jugar_duelo()
+    else:
+        jugar()
+
 
